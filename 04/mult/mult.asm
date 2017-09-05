016ecdef86c824e0936d0c993711e8598a821f36
Shiraj Ganguly <shirajganguly@gmail.com>
2017-09-04 23:48:59 -0400

    Basic Variable Initialization

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,23 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// @Number = RAM[0]
+// @Sum = 0
+// @Multiplier = RAM[1]
+//  While @Multiplier > 0
+//    @Sum = Sum + Number
+//    @Multiplier = @Multiplier - 1
+// END
+
+@Sum
+M=0
+@R0
+D=M
+@Number
+M=D
+@R1
+D=M
+@Multiplier
+M=D
+
