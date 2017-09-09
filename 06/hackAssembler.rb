89682a48957a182fe17186510edfc04d16f2d164
Shiraj Ganguly <shirajganguly@gmail.com>
2017-09-08 23:54:23 -0400

    Add the predefined symbols

--- 06/hackAssembler.rb
+++ 06/hackAssembler.rb
@@ -18,14 +18,14 @@ def test_encoder
   ]
 
   jump_expected_outputs = [
-   000,
-   001,
-   010,
-   011,
-   100,
-   101,
-   110,
-   111
+    000,
+    001,
+    010,
+    011,
+    100,
+    101,
+    110,
+    111
   ]
 
   dest_inputs = [
@@ -257,6 +257,33 @@ end
 
 class SymbolTable
 # Manages the symbol table
+  attr_reader :table
+  def initialize
+    @table = {
+      'SP': 0,
+      'LCL': 1,
+      'ARG': 2,
+      'THIS': 3,
+      'SCREEN': 16384,
+      'KBD': 24576,
+      'R0': 0,
+      'R1': 1,
+      'R2': 2,
+      'R3': 3,
+      'R4': 4,
+      'R5': 5,
+      'R6': 6,
+      'R7': 7,
+      'R8': 8,
+      'R9': 9,
+      'R10': 10,
+      'R11': 11,
+      'R12': 12,
+      'R13': 13,
+      'R14': 14,
+      'R15': 15
+    }
+  end
 
 end
 
