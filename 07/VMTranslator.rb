a90bdf9b3f49eff99a1ac59dd703bdd203ec34f7
Shiraj Ganguly <shirajganguly@gmail.com>
2017-09-17 16:05:16 -0400

    Test for SimpleAdd Passes

--- 07/VMTranslator.rb
+++ 07/VMTranslator.rb
@@ -21,26 +21,113 @@ module Parser
     elsif tokens[1] && tokens[2]
       parse_instruction(tokens)
     else
-      parse_operation(tokens[0])
+      self.send(tokens[0].to_sym)
     end
   end
 
   def self.parse_instruction(tokens)
-    {
+    convert_instruction({
       type: tokens[0],
       address: tokens[1],
       value: tokens[2]
-    }
+    })
   end
 
-  def self.parse_operation(token)
-    operations[token.to_sym]
+  def self.add
+    self.decrement_sp + %w(
+      //Add
+      @SP
+      A=M;
+      D=M;
+      A=A-1;
+      D=D+M;
+      M=D;
+    )
   end
 
-  def self.operations
-    {
-      'add': 'ADD ASM CODE'
-    }
+  def self.sub
+
+  end
+
+  def self.neg
+
+  end
+
+  def self.eq
+
+  end
+
+  def self.gt
+
+  end
+
+  def self.lt
+
+  end
+
+  def self.and
+
+  end
+
+  def self.or
+
+  end
+
+  def self.not
+
+  end
+
+  def self.increment_sp
+    ["@SP","M=M+1;"]
+  end
+
+  def self.decrement_sp
+    ["@SP","M=M-1;"]
+  end
+
+  def self.convert_instruction(parsed_instruction)
+    case parsed_instruction[:type]
+    when 'push'
+      [
+        "//#{parsed_instruction}",
+        "#{self.segments(parsed_instruction)}",
+        "@SP",
+        "A=M",
+        "M=D"
+      ] + increment_sp
+    when 'pop'
+      [
+        "//#{parsed_instruction}",
+        "@SP",
+        "A=M",
+        "D=M",
+        "@R5",
+        "M=D",
+        "@#{self.segments[parsed_instruction[:address].upcase.to_sym]}",
+        "D=#{parsed_instruction[:value]}",
+        "D=D+M",
+        "@R6",
+        "M=D",
+        "@R5",
+        "D=M",
+        "@R6",
+        "M=D"
+      ] + decrement_sp
+    end
+  end
+
+  def self.segments(instruction)
+    case instruction[:address]
+    when 'local'
+    when 'argument'
+    when 'this'
+    when 'that'
+    when 'pointer'
+    when 'temp'
+    when 'constant'
+      "@#{instruction[:value]}\nD=A;"
+    when 'static'
+    end
   end
 end
 
@@ -56,5 +143,9 @@ File.open(ARGV[0],'r') do |source|
   end
 end
 
-puts output.compact.reject{|line| line==''}
-# out_file = ARGV[0][0..ARGV[0].rindex('.')] + 'asm'
+puts output.compact.reject{|line| line==""}
+out_file = ARGV[0][0..ARGV[0].rindex('.')] + 'asm'
+File.open(out_file,'w') do |assembly|
+  assembly.write(output.compact.reject{|line| line==''}.join("\n"))
+end
+
