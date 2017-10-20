1095b7a91849cfd4448471a3e7da164268b2e629
Shiraj Ganguly <shirajganguly@gmail.com>
2017-10-19 22:04:37 -0400

    Basic Skeleton for classes

---
+++ 10/SyntaxAnalyzer.rb
@@ -0,0 +1,109 @@
+class JackTokenizer
+  def initialize(file)
+    @file = file
+  end
+
+  def has_more_tokens()
+    # Return a Boolean
+    return true
+  end
+
+  def advance
+  end
+
+  def token_type
+  end
+
+  def key_word
+  end
+
+  def symbol
+  end
+
+  def identifier
+  end
+
+  def int_val
+  end
+
+  def string_val
+  end
+end
+
+class CompilationEngine
+  def initialize(file)
+  end
+
+  def compile_class
+
+  end
+
+  def compile_class_var_dec
+
+  end
+
+  def compile_subroutine
+
+  end
+
+  def compile_parameter_list
+
+  end
+
+  def compile_var_dec
+  end
+
+  def compile_statements
+
+  end
+
+  def compile_do
+
+  end
+
+  def compile_let
+
+  end
+
+  def compile_while
+
+  end
+
+  def compile_return
+
+  end
+
+  def compile_if
+
+  end
+
+  def compile_expression
+
+  end
+
+  def compile_term
+
+  end
+
+  def compile_expression_list
+
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
